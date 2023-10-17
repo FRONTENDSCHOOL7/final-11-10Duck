fc63643c6123c0b942a65b5f089fc79f80760079
samweol <sejuyes@gmail.com>
2023-10-17 09:41:05 +0900

    Feat➕: 버튼 컴포넌트 개발

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import { styled } from "styled-components";
 
-export default function Button() {
-  return <CustomButton>버튼</CustomButton>;
+export default function Button(props) {
+  const { buttonText, disabled, size = "L", reversed, onClickHandler } = props;
+  return (
+    <ButtonStyle
+      disabled={disabled}
+      reversed={reversed}
+      size={size}
+      onClick={onClickHandler}
+    >
+      {buttonText}
+    </ButtonStyle>
+  );
 }
 
-const CustomButton = styled.button`
-  color: blue;
+const ButtonStyle = styled.button`
+  display: block;
+  color: ${(props) => (props.reversed ? "#767676" : "#FFFFFF")};
+  width: 100%;
+  max-width: ${(props) =>
+    props.size.toUpperCase() === "L"
+      ? "322px"
+      : props.size.toUpperCase() === "M"
+      ? "120px"
+      : props.size.toUpperCase() === "MS"
+      ? "90px"
+      : "56px"};
+  height: ${(props) =>
+    props.size.toUpperCase() === "L"
+      ? "44px"
+      : props.size.toUpperCase() === "M"
+      ? "34px"
+      : props.size.toUpperCase() === "MS"
+      ? "32px"
+      : "28px"};
+  border: none;
+  border-radius: 44px;
+
+  background-color: ${(props) =>
+    props.reversed ? "#FFFFFF" : props.disabled ? "#FFC7A7" : "#f26e22"};
+  cursor: ${(props) => (props.disabled ? "default" : "pointer")};
+  font-size: ${(props) => (props.size.toUpperCase() === "S" ? "12px" : "14px")};
 `;
