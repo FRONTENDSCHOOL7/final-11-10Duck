98acedf337b06a5e01353a931c9cec21a88b9f1c
samweol <sejuyes@gmail.com>
2023-10-17 09:42:59 +0900

    Style 🎨: Home.jsx에 스타일 확인용 코드 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../components/Button";
 
 export default function Home() {
-  const onClickHandler = () => {
-    console.log("Click!!");
-  };
   return (
     <>
       <div>씁덕학개론</div>
       <Link to="/signin">로그인</Link>
       <Link to="signup">회원가입</Link>
-      <Button buttonText={"로그인"} disabled={true} size="m" />
-      <Button buttonText={"로그인"} size="s" onClickHandler={onClickHandler} />
-      <Button
-        buttonText={"로그인"}
-        reversed={true}
-        onClickHandler={onClickHandler}
-      />
     </>
   );
 }
