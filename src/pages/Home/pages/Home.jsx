dbe64eb625e5f29439ac1e141025686f8d4e5ed1
seoyeon.lee <ivyseo0312@korea.ac.kr>
2023-10-18 17:02:10 +0900

    Test 🚧: input 컴포넌트 확인을 위해 홈화면에서 보이게 추가

--- src/pages/Home/pages/Home.jsx
+++ src/pages/Home/pages/Home.jsx
@@ -4,6 +4,7 @@ import Button from "../../../components/Button";
 import Layout from "../../../components/Layout";
 import MainHeader from "../../../components/Header/MainHeader";
 import NavBar from "../../../components/Footer/NavBar";
+import Input from "../../../components/Input";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Link to="/signin">로그인</Link>
         <Link to="signup">회원가입</Link>
         <Button />
+        <Input />
       </div>
 
       <NavBar />
