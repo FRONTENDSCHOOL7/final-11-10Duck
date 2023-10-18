e148ae752c72e592d4efb311f7c45db191475715
seoyeon.lee <ivyseo0312@korea.ac.kr>
2023-10-18 19:47:51 +0900

    Refactor ♻️: 코드 간결하게 정리

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,28 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import { COLOR } from "../utils";
 
-export default function Input() {
+export default function Input2(props) {
+  const { main, search } = props;
+
   return (
-    <>
-      <StyledInput type="text" placeholder="Main Input" main />
-      <StyledInput type="text" placeholder="Search Input" search />
-      <StyledInput type="text" placeholder="Chat Input" />
-    </>
+    <StyledInput
+      type="text"
+      placeholder="Chat Input"
+      main={main}
+      search={search}
+    />
   );
 }
 
 const StyledInput = styled.input`
   outline: none;
   width: 322px;
-  padding: ${(props) =>
-    props.search ? "5px 10px 5px 10px" : "5px 2px 5px 2px"};
+  padding: 5px 2px 5px 2px;
   border: 1px solid ${COLOR.borderPrimaryColor};
-  border-width: ${(props) => (props.main ? "0 0 1px" : "0")};
-  background-color: ${(props) =>
-    props.search ? COLOR.bgSecondaryColor : COLOR.bgPrimaryColor};
-  border-radius: ${(props) => (props.search ? "2rem" : "0")};
+  border-width: 0;
+  background-color: ${COLOR.bgPrimaryColor};
+  border-radius: 0;
   &:focus {
     border: 1px solid ${COLOR.borderSecondaryColor};
-    border-width: ${(props) => (props.main ? " 0 0 1px" : "1px")};
+    border-width: 1px;
   }
+
+  ${(props) => props.main && `border-width: 0 0 1px;`}
+  ${(props) =>
+    props.search &&
+    `padding: 5px 10px 5px 10px;
+    background-color: ${COLOR.bgSecondaryColor};
+    border-radius: 2rem;`}
 `;
