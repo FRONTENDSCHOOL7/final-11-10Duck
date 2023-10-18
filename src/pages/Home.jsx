b5e41a405e5a021362f153392a1e7ff8427ba52a
oqoqoh <oqoq59@gmail.com>
2023-10-18 22:19:22 +0900

    Refactor ♻️: Layout.jsx 내부 Context 부분 리펙토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,18 @@ import Button from '../components/Button';
 import Layout from '../components/Layout';
 import MainHeader from '../components/Header/MainHeader';
 import NavBar from '../components/Footer/NavBar';
+import Content from '../components/Content';
 
 export default function Home() {
     return (
         <Layout>
             <MainHeader />
 
-            <div>
-                <div>씁덕학개론</div>
+            <Content>
                 <Link to="/signin">로그인</Link>
                 <Link to="signup">회원가입</Link>
                 <Button />
-            </div>
+            </Content>
 
             <NavBar />
         </Layout>
