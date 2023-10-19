b0e7ec5f88b1c3027787f0a6981d8f3603d4b6f9
samweol <sejuyes@gmail.com>
2023-10-19 17:54:12 +0900

    Style 🎨: 불필요한 route 삭제

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home/pages/Home";
 import Feed from "../pages/Home/pages/Feed";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Feed />} />
-      <Route path="/signin-select" element={<Home />} />
     </Routes>
   );
 }
