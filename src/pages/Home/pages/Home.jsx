b28ce0a28e36ffbd80510aa9fca415c8da463265
oqoqoh <oqoq59@gmail.com>
2023-10-19 14:18:49 +0900

    Rename 🚚 : Home.jsx에 Layout.jsx import 경로 수정

--- src/pages/Home/pages/Home.jsx
+++ src/pages/Home/pages/Home.jsx
@@ -1,35 +1,31 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Button from "../../../components/Button";
-import Layout from "../../../components/Layout";
-import MainHeader from "../../../components/Header/MainHeader";
-import NavBar from "../../../components/Footer/NavBar";
-import Input from "../../../components/Input/Input";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../../components/Button';
+import Layout from '../../../components/Layout/Layout';
+import MainHeader from '../../../components/Header/MainHeader';
+import NavBar from '../../../components/Footer/NavBar';
+import Input from '../../../components/Input/Input';
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const onChangeHandler = (email) => {
-    setEmail(email);
-  };
+    const [email, setEmail] = useState('');
+    const onChangeHandler = (email) => {
+        setEmail(email);
+    };
 
-  return (
-    <Layout>
-      <MainHeader />
+    return (
+        <Layout>
+            <MainHeader />
 
-      <div>
-        <div>씁덕학개론</div>
-        <Link to="/signin">로그인</Link>
-        <Link to="signup">회원가입</Link>
-        <Button />
-        <Input
-          type="text"
-          labelText="이메일"
-          onChangeHandler={onChangeHandler}
-        />
-        <Input type="password" labelText="비밀번호" />
-      </div>
+            <div>
+                <div>씁덕학개론</div>
+                <Link to="/signin">로그인</Link>
+                <Link to="signup">회원가입</Link>
+                <Button />
+                <Input type="text" labelText="이메일" onChangeHandler={onChangeHandler} />
+                <Input type="password" labelText="비밀번호" />
+            </div>
 
-      <NavBar />
-    </Layout>
-  );
+            <NavBar />
+        </Layout>
+    );
 }
