9cf84ac0680b0560f12e5a4f4dd7bddfcd611b47
oqoqoh <oqoq59@gmail.com>
2023-10-19 14:30:33 +0900

    Rename 🚚 : Home.jsx에 LayoutContent 예시 적용

--- src/pages/Home/pages/Home.jsx
+++ src/pages/Home/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../../components/Button';
 import Layout from '../../../components/Layout/Layout';
+import LayoutContent from '../../../components/Layout/LayoutContent';
 import MainHeader from '../../../components/Header/MainHeader';
 import NavBar from '../../../components/Footer/NavBar';
 import Input from '../../../components/Input/Input';
@@ -13,17 +14,17 @@ export default function Home() {
     };
 
     return (
-        <Layout>
+        <Layout bgColor={'white'}>
             <MainHeader />
 
-            <div>
+            <LayoutContent>
                 <div>씁덕학개론</div>
                 <Link to="/signin">로그인</Link>
                 <Link to="signup">회원가입</Link>
                 <Button />
                 <Input type="text" labelText="이메일" onChangeHandler={onChangeHandler} />
                 <Input type="password" labelText="비밀번호" />
-            </div>
+            </LayoutContent>
 
             <NavBar />
         </Layout>
