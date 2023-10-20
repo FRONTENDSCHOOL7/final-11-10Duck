5710b358d80ae32b46770117ca62312f95ffef33
samweol <sejuyes@gmail.com>
2023-10-20 23:24:16 +0900

    Feat➕: 바텀 탭 클릭 시 해당 페이지로 이동하는 기능 useNavigate 이용하여 추가

--- src/components/Footer/NavBar.jsx
+++ src/components/Footer/NavBar.jsx
@@ -1,53 +1,60 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import { ReactComponent as HomeIcon } from '../../assets/icon/icon-home.svg';
-import { ReactComponent as ChatIcon } from '../../assets/icon/icon-message-circle.svg';
-import { ReactComponent as PostIcon } from '../../assets/icon/icon-edit.svg';
-import { ReactComponent as ProfileIcon } from '../../assets/icon/icon-user.svg';
-import { COLOR, FONT_SIZE } from '../../utils';
+import React from "react";
+import { styled } from "styled-components";
+import { ReactComponent as HomeIcon } from "../../assets/icon/icon-home.svg";
+import { ReactComponent as ChatIcon } from "../../assets/icon/icon-message-circle.svg";
+import { ReactComponent as PostIcon } from "../../assets/icon/icon-edit.svg";
+import { ReactComponent as ProfileIcon } from "../../assets/icon/icon-user.svg";
+import { COLOR, FONT_SIZE } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
-    const menus = [
-        { label: '홈', icon: HomeIcon },
-        { label: '채팅', icon: ChatIcon },
-        { label: '게시물 작성', icon: PostIcon },
-        { label: '프로필', icon: ProfileIcon },
-    ];
-    // 아이콘 클릭시 svg fill stroke 변경 기능 필요
-    return (
-        <NavBarContainer>
-            {menus.map((nav) => {
-                return (
-                    <NavStyle key={nav.label}>
-                        <nav.icon width={24} height={24} />
-                        <NavText>{nav.label}</NavText>
-                    </NavStyle>
-                );
-            })}
-        </NavBarContainer>
-    );
+  const navigate = useNavigate();
+  const menus = [
+    { label: "홈", icon: HomeIcon, url: "/" },
+    { label: "채팅", icon: ChatIcon, url: "/chat" },
+    { label: "게시물 작성", icon: PostIcon, url: "/post/upload" },
+    { label: "프로필", icon: ProfileIcon, url: "profile" },
+  ];
+  // 아이콘 클릭시 svg fill stroke 변경 기능 필요
+  return (
+    <NavBarContainer>
+      {menus.map((nav) => {
+        return (
+          <NavStyle
+            key={nav.label}
+            onClick={() => {
+              navigate(nav.url);
+            }}
+          >
+            <nav.icon width={24} height={24} />
+            <NavText>{nav.label}</NavText>
+          </NavStyle>
+        );
+      })}
+    </NavBarContainer>
+  );
 }
 
 const NavBarContainer = styled.footer`
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 14px;
-    border-top: 1px solid ${COLOR.bgBorderColor};
-    background-color: ${COLOR.bgPrimaryColor};
-    box-sizing: border-box;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+  border-top: 1px solid ${COLOR.bgBorderColor};
+  background-color: ${COLOR.bgPrimaryColor};
+  box-sizing: border-box;
 `;
 
 const NavStyle = styled.button`
-    width: 84px;
-    border: none;
-    background: none;
-    cursor: pointer;
+  width: 84px;
+  border: none;
+  background: none;
+  cursor: pointer;
 `;
 
 const NavText = styled.p`
-    color: ${COLOR.fontPrimaryColor};
-    font-size: ${FONT_SIZE.small};
-    margin-top: 4px;
+  color: ${COLOR.fontPrimaryColor};
+  font-size: ${FONT_SIZE.small};
+  margin-top: 4px;
 `;
