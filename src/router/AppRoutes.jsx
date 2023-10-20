2fb5ad866a7b19cd80702bab26200b4f516da2e0
oqoqoh <oqoq59@gmail.com>
2023-10-20 14:16:05 +0900

    Feat➕: AppRoutes.jsx에 /profile 추가

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Post from "../pages/Post/pages/Post";
-import PostUpload from "../pages/PostUpload/pages/PostUpload";
-import AddProduct from "../pages/AddProduct/pages/AddProduct";
-import Feed from "../pages/Home/pages/Feed";
-
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Post from '../pages/Post/pages/Post';
+import PostUpload from '../pages/PostUpload/pages/PostUpload';
+import AddProduct from '../pages/AddProduct/pages/AddProduct';
+import Feed from '../pages/Home/pages/Feed';
+import Profile from '../pages/Profile/pages/Profile';
 
 export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Feed />} />
-      <Route path="/post" element={<Post />} />
-      <Route path="/post/upload" element={<PostUpload />} />  
-      <Route path="/add-product" element={<AddProduct />} />  
+    return (
+        <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/post/upload" element={<PostUpload />} />
+            <Route path="/add-product" element={<AddProduct />} />
 
-    </Routes>
-  );
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    );
 }
