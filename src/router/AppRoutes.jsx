2c0c6f721495349e0b23b9b0f358ad5d36bf1c93
seoyeon.lee <ivyseo0312@korea.ac.kr>
2023-10-20 19:34:13 +0900

    Feat➕: AppRoutes.jsx에 /signup 추가

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -4,17 +4,16 @@ import Post from "../pages/Post/pages/Post";
 import PostUpload from "../pages/PostUpload/pages/PostUpload";
 import AddProduct from "../pages/AddProduct/pages/AddProduct";
 import Feed from "../pages/Home/pages/Feed";
-
-
+import Signup from "../pages/Signup/pages/Signup";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Feed />} />
       <Route path="/post" element={<Post />} />
-      <Route path="/post/upload" element={<PostUpload />} />  
-      <Route path="/add-product" element={<AddProduct />} />  
-
+      <Route path="/post/upload" element={<PostUpload />} />
+      <Route path="/add-product" element={<AddProduct />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
