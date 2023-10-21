19f22ba2d09f56ff70ef35a59a01e89c20eea424
samweol <sejuyes@gmail.com>
2023-10-21 19:18:51 +0900

    Feat➕: 게시물 등록하기 기능 추가 이미지 없이 텍스트만 등록했을 때 고려 게시물 내용 여부에 따라 업로드 버튼 활성화 고려 header에 props이름 오타 있어서 수정 게시물 등록 완료 시 해당 게시글로 이동하기까지 (데이터 뿌려주기는 아직)

--- src/pages/PostUpload/pages/PostUpload.jsx
+++ src/pages/PostUpload/pages/PostUpload.jsx
@@ -1,19 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../../components/Layout/Layout";
 import UploadHeader from "../../../components/Header/UploadHeader";
 import ProfileImage from "../../../assets/basic-profile-img.png";
 import { styled } from "styled-components";
 import ImageButton from "../components/ImageButton";
 import LayoutContent from "../../../components/Layout/LayoutContent";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function PostUpload() {
+  const [isButtonActive, setIsButtonAcitve] = useState(false);
+  const [content, setContent] = useState({ text: "", image: "" });
+  const navigate = useNavigate();
+
+  /**
+   * 게시물 등록하기 함수, 현재 이미지 없이 등록하는 것으로만 들어가있음
+   */
+  const uploadPost = async () => {
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}post`,
+        {
+          post: {
+            content: content.text,
+            image: content.image,
+          },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MmY4YmQ1YjJjYjIwNTY2MzdjNDZmNiIsImV4cCI6MTcwMjc5OTAzMSwiaWF0IjoxNjk3NjE1MDMxfQ.osT2yHu_EcI0sjl8wLqbGJ08zfnaL0aArmHcU_PnfCA"}`,
+          },
+        }
+      );
+      navigate("/post", { state: { post: res.data } });
+    } catch (err) {
+      console.error(err);
+      // 시간 남으면 게시글 등록 실패시 모달창 띄워주기
+    }
+  };
+
+  useEffect(() => {
+    if (!!content.text.length) {
+      setIsButtonAcitve(false);
+    } else {
+      setIsButtonAcitve(true);
+    }
+  }, [content]);
+
   return (
     <Layout>
-      <UploadHeader />
+      <UploadHeader
+        buttonText={"업로드"}
+        disabled={isButtonActive}
+        onClickHandler={uploadPost}
+      />
       <LayoutContent>
         <ContainerStyle>
           <ProfileImageStyle src={ProfileImage} alt="프로필 이미지" />
-          <TextAreaContainerStyle placeholder="게시물 입력하기"></TextAreaContainerStyle>
+          <TextAreaContainerStyle
+            placeholder="게시물 입력하기"
+            onChange={(event) => {
+              setContent({ ...content, text: event.target.value });
+            }}
+          ></TextAreaContainerStyle>
         </ContainerStyle>
         <ImageButton />
       </LayoutContent>
