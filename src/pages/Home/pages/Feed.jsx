a0737abfee7b862398a0980efbef76b65e26dd29
egyeong <egyeong49@gmail.com>
2023-10-22 02:49:31 +0900

    Feat➕ : Feed.jsx에 follow가 있을 경우의 페이지 추가

--- src/pages/Home/pages/Feed.jsx
+++ src/pages/Home/pages/Feed.jsx
@@ -1,53 +1,23 @@
-import { styled } from "styled-components";
 import MainHeader from "../../../components/Header/MainHeader";
-import NavBar from "../../../components/Footer/NavBar";
 import Layout from "../../../components/Layout/Layout";
 import LayoutContent from "../../../components/Layout/LayoutContent";
-import SearchIcon from "../../../assets/icon/icon-symbol-logo-gray.png";
-import Button from "../../../components/Button";
+import PostItem from "../../../components/Post";
+import FeedNone from "../components/FeedNone";
 
-export default function Feed() {
-  return (
-    <Layout>
-      <MainHeader />
-      <LayoutContent>
-        <FeedPage>
-          <IconImg src={SearchIcon} alt="로고 아이콘" />
-          <span className="msg">유저를 검색해 팔로우 해보세요!</span>
+export default function FeedFollow(props) {
+  // const { profileImg, userName, userId, postImg, postContent, heartCount, commentCount } = props;
+  const { user, follow = 1 } = props;
 
-          {/* onClick 구현 필요 */}
-          <Button
-            buttonText={"검색하기"}
-            disabled={false}
-            size={"M"}
-            reversed={false}
-            onClick={() => {}}
-          />
-        </FeedPage>
-      </LayoutContent>
-      <NavBar />
-    </Layout>
-  );
+  // follow에 대한 정보를 어떻게 넘길지... 수정 필요
+
+  if (follow === 0) return <FeedNone />;
+  else if (follow > 0)
+    return (
+      <Layout>
+        <MainHeader />
+        <LayoutContent>
+          <PostItem />
+        </LayoutContent>
+      </Layout>
+    );
 }
-
-const FeedPage = styled.div`
-  margin-top: 50%;
-
-  & > .msg {
-    display: block;
-    padding: 20px;
-    margin: 0 auto;
-    text-align: center;
-  }
-  & > Button {
-    margin: 0 auto;
-  }
-`;
-
-const IconImg = styled.img`
-  display: block;
-  width: 100px;
-  height: 100px;
-  /* cursor: pointer; */
-  margin: 0 auto;
-`;
