59e29222901ee2c8ca4a442fd86ead2b8d70eb2f
oqoqoh <oqoq59@gmail.com>
2023-10-21 22:50:15 +0900

    Design 💄 : ProfilInfo.jsx 퍼블리싱 완료

--- src/pages/Profile/pages/Profile.jsx
+++ src/pages/Profile/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../../../components/Layout/Layout';
 import BasicHeader from '../../../components/Header/BasicHeader';
 import LayoutContent from '../../../components/Layout/LayoutContent';
@@ -7,12 +7,20 @@ import PostItem from '../../../components/Post';
 import ProfileInfo from '../components/ProfileInfo';
 
 export default function Profile() {
+    const [whosProfile, setWhosProfile] = useState('');
+    useEffect(() => {
+        // 본인 프로필인지 아니면 타인의 프로필,팔로우 여부 파악해서
+        // myProfile, isFollow, notFollow로
+        // whosProfile 변경
+    }, []);
     return (
         <Layout>
             <BasicHeader />
             <LayoutContent isWhite={false}>
-                <ProfileInfo />
-
+                {/* 프로필 정보 */}
+                <ProfileInfo whosProfile={'isFollow'} />
+                {/* 판매 중인 상품 */}
+                {/* 포스트한 게시물 */}
                 <PostItem />
             </LayoutContent>
             <NavBar />
