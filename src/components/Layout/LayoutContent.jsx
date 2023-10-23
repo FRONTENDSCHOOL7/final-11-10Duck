8f9829d959ac9b252175be7d1046a5936f95a1bd
oqoqoh <oqoq59@gmail.com>
2023-10-23 15:02:10 +0900

    Refactor ♻️: LayoutContent.jsx에 패딩값 props로 전달 추가

--- src/components/Layout/LayoutContent.jsx
+++ src/components/Layout/LayoutContent.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { styled } from 'styled-components';
 import { COLOR } from '../../utils';
 
-export default function LayoutContent({ children, isWhite }) {
-    return <ContentStyle isWhite={isWhite}> {children}</ContentStyle>;
+export default function LayoutContent({ children, isWhite, paddingOn }) {
+    return (
+        <ContentStyle isWhite={isWhite} paddingOn={paddingOn}>
+            {children}
+        </ContentStyle>
+    );
 }
 
 const ContentStyle = styled.main`
     flex-grow: 1;
-    padding: 0 16px;
+    padding: 0 ${(props) => (props.paddingOn ? `16px` : `0`)};
     background-color: ${(props) => (props.isWhite === undefined ? COLOR.bgPrimaryColor : props.isWhite ? COLOR.bgPrimaryColor : COLOR.bgSecondaryColor)};
     position: relative;
     overflow: scroll;
+
+    // scrollbar 숨기기
+    ::-webkit-scrollbar {
+        display: none;
+    }
 `;
