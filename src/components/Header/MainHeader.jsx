5da15432de79ba825c93a6f0682b1955af83137d
samweol <sejuyes@gmail.com>
2023-10-24 15:47:16 +0900

    Feat➕: 헤더에서 아이콘 클릭시 검색 페이지로 이동

--- src/components/Header/MainHeader.jsx
+++ src/components/Header/MainHeader.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
-import { styled } from 'styled-components';
-import SearchIcon from '../../assets/icon/icon-search.png';
-import { COLOR } from '../../utils';
+import React from "react";
+import { styled } from "styled-components";
+import SearchIcon from "../../assets/icon/icon-search.png";
+import { COLOR } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
 export default function MainHeader() {
-    return (
-        <HeaderContainer>
-            <MarketTitle>씁덕학개론 피드</MarketTitle>
-            <IconImg src={SearchIcon} alt="검색하기 아이콘" />
-        </HeaderContainer>
-    );
+  const navigate = useNavigate();
+  return (
+    <HeaderContainer>
+      <MarketTitle>씁덕학개론 피드</MarketTitle>
+      <IconImg
+        src={SearchIcon}
+        alt="검색하기 아이콘"
+        onClick={() => {
+          navigate("/search");
+        }}
+      />
+    </HeaderContainer>
+  );
 }
 const HeaderContainer = styled.header`
-    height: 48px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px solid ${COLOR.bgBorderColor};
-    background-color: ${COLOR.bgPrimaryColor};
-    padding: 13px 16px;
-    box-sizing: border-box;
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid ${COLOR.bgBorderColor};
+  background-color: ${COLOR.bgPrimaryColor};
+  padding: 13px 16px;
+  box-sizing: border-box;
 `;
 const IconImg = styled.img`
-    width: 22px;
-    height: 22px;
-    cursor: pointer;
-    margin-left: 0;
+  width: 22px;
+  height: 22px;
+  cursor: pointer;
+  margin-left: 0;
 `;
 const MarketTitle = styled.h2`
-    color: #000;
-    font-size: 18px;
-    font-weight: 500;
+  color: #000;
+  font-size: 18px;
+  font-weight: 500;
 `;
