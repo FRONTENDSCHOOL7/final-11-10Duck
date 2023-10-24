b230a95e1706bc6285d845ddc4d6d1cd7be89251
samweol <sejuyes@gmail.com>
2023-10-24 13:54:51 +0900

    Feat➕: 댓글 달기 기능 추가
    - 댓글 추가 시 화면에 바로 렌더링되게 하기
    - 댓글 추가 기능

--- src/pages/Post/pages/Post.jsx
+++ src/pages/Post/pages/Post.jsx
@@ -10,29 +10,76 @@ import useAPI from "../../../hooks/useAPI";
 
 export default function Post() {
   const [post, setPost] = useState();
+  const [commentList, setCommentList] = useState([]);
+  const [comment, setComment] = useState("");
   const { header } = useAPI();
+
+  const postId = "6536095db2cb205663850892";
   const fetchPost = async () => {
     try {
       const res = await axios.get(
-        `${process.env.REACT_APP_API_URL}post/6536095db2cb205663850892`,
+        `${process.env.REACT_APP_API_URL}post/${postId}`,
         {
           headers: header,
         }
       );
+      console.log("🌟게시글불러오기 성공");
       setPost(res.data);
     } catch (err) {
+      console.log("🔥게시글 불러오기 실패");
+      console.error(err);
+    }
+  };
+
+  const fetchComment = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}post/${postId}/comments`,
+        {
+          headers: header,
+        }
+      );
+
+      console.log("🌟댓글불러오기 성공");
+      setCommentList(res.data.comments);
+    } catch (err) {
+      console.log("🔥댓글불러오기 실패");
       console.error(err);
     }
   };
 
   const uploadComment = async () => {
     try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}post/${postId}/comments`,
+        {
+          comment: {
+            content: "test",
+          },
+        },
+        {
+          headers: header,
+        }
+      );
+
+      console.log("🌟댓글달기 성공");
+      console.log(res);
     } catch (err) {
+      console.log("🔥댓글달기 실패");
       console.error(err);
+    } finally {
+      setComment("");
+      fetchComment();
     }
   };
+
+  const onChangeHandler = (content) => {
+    setComment(content);
+  };
+
   useEffect(() => {
     fetchPost();
+    fetchComment();
   }, []);
 
   if (!!post) {
@@ -41,9 +88,18 @@ export default function Post() {
         <BasicHeader />
         <LayoutContent>
           <PostItem post={post} />
-          <Comment />
+          {commentList.map((item) => (
+            <Comment />
+          ))}
         </LayoutContent>
-        <CommentBar mode="post" />
+        <CommentBar
+          mode="post"
+          content={comment}
+          onSubmitHandler={uploadComment}
+          onChangeHandler={(e) => {
+            onChangeHandler(e.target.value);
+          }}
+        />
       </Layout>
     );
   }
