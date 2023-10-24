13f03744c39b4fb7f2a4a2ef02e25acbe7083ee1
samweol <sejuyes@gmail.com>
2023-10-24 10:45:50 +0900

    Feat➕: 판매상품 컴포넌트 제작

--- src/pages/Profile/pages/Profile.jsx
+++ src/pages/Profile/pages/Profile.jsx
@@ -1,29 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import Layout from '../../../components/Layout/Layout';
-import BasicHeader from '../../../components/Header/BasicHeader';
-import LayoutContent from '../../../components/Layout/LayoutContent';
-import NavBar from '../../../components/Footer/NavBar';
-import ProfileInfo from '../components/ProfileInfo';
-import PostList from '../components/PostList';
+import React, { useEffect, useState } from "react";
+import Layout from "../../../components/Layout/Layout";
+import BasicHeader from "../../../components/Header/BasicHeader";
+import LayoutContent from "../../../components/Layout/LayoutContent";
+import NavBar from "../../../components/Footer/NavBar";
+import ProfileInfo from "../components/ProfileInfo";
+import PostList from "../components/PostList";
+import ProductScroller from "../../../components/Product/ProductScroller";
+import { res } from "../../../constants/product";
 
 export default function Profile() {
-    const [whosProfile, setWhosProfile] = useState('');
-    useEffect(() => {
-        // 본인 프로필인지 아니면 타인의 프로필,팔로우 여부 파악해서
-        // setWhosProfile( myProfile || isFollow || notFollow )
-        // whosProfile 변경
-    }, []);
-    return (
-        <Layout>
-            <BasicHeader />
-            <LayoutContent isWhite={false} paddingOff={true}>
-                {/* 프로필 정보 */}
-                <ProfileInfo whosProfile={'notFollow'} />
-                {/* 판매 중인 상품 */}
-                {/* 포스트한 게시물 */}
-                <PostList />
-            </LayoutContent>
-            <NavBar />
-        </Layout>
-    );
+  const [whosProfile, setWhosProfile] = useState("");
+  useEffect(() => {
+    // 본인 프로필인지 아니면 타인의 프로필,팔로우 여부 파악해서
+    // setWhosProfile( myProfile || isFollow || notFollow )
+    // whosProfile 변경
+  }, []);
+  return (
+    <Layout>
+      <BasicHeader />
+      <LayoutContent isWhite={false} paddingOff={true}>
+        {/* 프로필 정보 */}
+        <ProfileInfo whosProfile={"notFollow"} />
+        {/* 판매 중인 상품 */}
+        <ProductScroller products={res.product} />
+        {/* 포스트한 게시물 */}
+        <PostList />
+      </LayoutContent>
+      <NavBar />
+    </Layout>
+  );
 }
