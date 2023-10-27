9d300601ac0eec2ee1440584eada394772239a0a
oqoqoh <oqoq59@gmail.com>
2023-10-27 15:19:08 +0900

    Design 💄: 프로필-수정 페이지 css 수정

--- src/pages/Profile/components/ProfileInfo.jsx
+++ src/pages/Profile/components/ProfileInfo.jsx
@@ -1,241 +1,222 @@
-import React, { useEffect, useState } from "react";
-import { styled } from "styled-components";
-import { COLOR, FONT_SIZE } from "../../../utils";
-import Button from "../../../components/Button";
-import chatIcon from "../../../assets/icon/icon-message-circle.svg";
-import shareIcon from "../../../assets/icon/icon-share.png";
-import { useNavigate } from "react-router-dom";
-import useAPI from "../../../hooks/useAPI";
-import { api } from "../../../api/baseURL";
+import React, { useEffect, useState } from 'react';
+import { styled } from 'styled-components';
+import { COLOR, FONT_SIZE } from '../../../utils';
+import Button from '../../../components/Button';
+import chatIcon from '../../../assets/icon/icon-message-circle.svg';
+import shareIcon from '../../../assets/icon/icon-share.png';
+import { useNavigate } from 'react-router-dom';
+import useAPI from '../../../hooks/useAPI';
+import { api } from '../../../api/baseURL';
 
-export default function ProfileInfo({
-  isMyProfile,
-  profileInfo,
-  isFollow,
-  changeProfileInfo,
-  changeIsFollow,
-}) {
-  const { header } = useAPI();
-  const navigate = useNavigate();
+export default function ProfileInfo({ isMyProfile, profileInfo, isFollow, changeProfileInfo, changeIsFollow }) {
+    const { header } = useAPI();
+    const navigate = useNavigate();
 
-  //const [userInfo, setUserInfo] = useState(profileInfo ? profileInfo : {});
-  //const [isFollow, setIsFollow] = useState();
+    //const [userInfo, setUserInfo] = useState(profileInfo ? profileInfo : {});
+    //const [isFollow, setIsFollow] = useState();
 
-  const handleFollowingsClick = () => {
-    navigate(`/profile/${profileInfo.accountname}/followings`);
-  };
-  const handleFollowersClick = () => {
-    navigate(`/profile/${profileInfo.accountname}/followers`);
-  };
+    const handleFollowingsClick = () => {
+        navigate(`/profile/${profileInfo.accountname}/followings`);
+    };
+    const handleFollowersClick = () => {
+        navigate(`/profile/${profileInfo.accountname}/followers`);
+    };
 
-  const fetchDoFollow = async () => {
-    try {
-      const res = await api.post(`/profile/${profileInfo.accountname}/follow`, {
-        headers: header,
-      });
-      console.log("🌟 팔로우하기 성공");
-      //changeProfileInfo(res.data.profile);
-      //setIsFollow(res.data.profile.isfollow);
-    } catch (err) {
-      console.error(err);
-      console.log("🔥 팔로우하기 실패");
-    }
-  };
-
-  const fetchDoUnfollow = async () => {
-    try {
-      const res = await api.post(
-        `/profile/${profileInfo.accountname}/unfollow`,
-        {
-          headers: header,
+    const fetchDoFollow = async () => {
+        try {
+            const res = await api.post(`/profile/${profileInfo.accountname}/follow`, {
+                headers: header,
+            });
+            console.log('🌟 팔로우하기 성공');
+            //changeProfileInfo(res.data.profile);
+            //setIsFollow(res.data.profile.isfollow);
+        } catch (err) {
+            console.error(err);
+            console.log('🔥 팔로우하기 실패');
         }
-      );
-      console.log("🌟 언팔로우하기 성공");
-      //changeProfileInfo(res.data.profile);
-      //setIsFollow(res.data.profile.isfollow);
-    } catch (err) {
-      console.error(err);
-      console.log("🔥 언팔로우하기 실패");
-    }
-  };
-  const handleFollowClick = () => {
-    //fetchDoFollow();
-    changeIsFollow((prev) => !prev);
-  };
-  const handleUnfollowClick = () => {
-    //fetchDoUnfollow();
-    changeIsFollow((prev) => !prev);
-  };
+    };
 
-  return (
-    <ProfileInfoContainer>
-      <h2 className="a11y-hidden">프로필 정보</h2>
-      <ProfileImgStyle>
-        <FollowInfoStyle onClick={handleFollowersClick}>
-          <FollowInfoNumbers isFollowing={true}>
-            {profileInfo.followerCount}
-          </FollowInfoNumbers>
-          <FollowInfoText>followers</FollowInfoText>
-        </FollowInfoStyle>
-        <ProfileImage src={profileInfo.image} alt="유저 프로필 이미지" />
-        <FollowInfoStyle onClick={handleFollowingsClick}>
-          <FollowInfoNumbers>{profileInfo.followingCount}</FollowInfoNumbers>
-          <FollowInfoText>followings</FollowInfoText>
-        </FollowInfoStyle>
-      </ProfileImgStyle>
-      <ProfileName>{profileInfo.username}</ProfileName>
-      <ProfileId>@{profileInfo.accountname}</ProfileId>
-      <ProfileMessage>{profileInfo.intro}</ProfileMessage>
-      <ButtonsStyle>
-        {isMyProfile ? (
-          <>
-            <MyProfileBtns>
-              <Button
-                buttonText={"프로필 수정"}
-                reversed
-                size={"M"}
-                onClickHandler={() => {}}
-              />
-              <Button
-                buttonText={"상품 등록"}
-                reversed
-                size={"M"}
-                onClickHandler={() => {
-                  navigate("/add-product");
-                }}
-              />
-            </MyProfileBtns>
-          </>
-        ) : (
-          <>
-            <ChatIcon src={chatIcon} alt="채팅 아이콘" />
-            {/* {profileInfo.isfollow ? (
+    const fetchDoUnfollow = async () => {
+        try {
+            const res = await api.post(`/profile/${profileInfo.accountname}/unfollow`, {
+                headers: header,
+            });
+            console.log('🌟 언팔로우하기 성공');
+            //changeProfileInfo(res.data.profile);
+            //setIsFollow(res.data.profile.isfollow);
+        } catch (err) {
+            console.error(err);
+            console.log('🔥 언팔로우하기 실패');
+        }
+    };
+    const handleFollowClick = () => {
+        //fetchDoFollow();
+        changeIsFollow((prev) => !prev);
+    };
+    const handleUnfollowClick = () => {
+        //fetchDoUnfollow();
+        changeIsFollow((prev) => !prev);
+    };
+
+    return (
+        <ProfileInfoContainer>
+            <h2 className="a11y-hidden">프로필 정보</h2>
+            <ProfileImgStyle>
+                <FollowInfoStyle onClick={handleFollowersClick}>
+                    <FollowInfoNumbers isFollowing={true}>{profileInfo.followerCount}</FollowInfoNumbers>
+                    <FollowInfoText>followers</FollowInfoText>
+                </FollowInfoStyle>
+                <ProfileImage src={profileInfo.image} alt="유저 프로필 이미지" />
+                <FollowInfoStyle onClick={handleFollowingsClick}>
+                    <FollowInfoNumbers>{profileInfo.followingCount}</FollowInfoNumbers>
+                    <FollowInfoText>followings</FollowInfoText>
+                </FollowInfoStyle>
+            </ProfileImgStyle>
+            <ProfileName>{profileInfo.username}</ProfileName>
+            <ProfileId>@{profileInfo.accountname}</ProfileId>
+            <ProfileMessage>{profileInfo.intro}</ProfileMessage>
+            <ButtonsStyle>
+                {isMyProfile ? (
+                    <>
+                        <MyProfileBtns>
+                            <Button
+                                buttonText={'프로필 수정'}
+                                reversed
+                                size={'M'}
+                                onClickHandler={() => {
+                                    navigate('/profile/edit');
+                                }}
+                            />
+                            <Button
+                                buttonText={'상품 등록'}
+                                reversed
+                                size={'M'}
+                                onClickHandler={() => {
+                                    navigate('/add-product');
+                                }}
+                            />
+                        </MyProfileBtns>
+                    </>
+                ) : (
+                    <>
+                        <ChatIcon src={chatIcon} alt="채팅 아이콘" />
+                        {/* {profileInfo.isfollow ? (
                             <Button buttonText={'언팔로우하기'} reversed size={'M'} onClickHandler={handleUnfollowClick} />
                         ) : (
                             <Button buttonText={'팔로우하기'} size={'M'} onClickHandler={handleFollowClick} />
                         )} */}
-            {isFollow ? (
-              <Button
-                buttonText={"언팔로우하기"}
-                reversed
-                size={"M"}
-                onClickHandler={handleUnfollowClick}
-              />
-            ) : (
-              <Button
-                buttonText={"팔로우하기"}
-                size={"M"}
-                onClickHandler={handleFollowClick}
-              />
-            )}
+                        {isFollow ? (
+                            <Button buttonText={'언팔로우하기'} reversed size={'M'} onClickHandler={handleUnfollowClick} />
+                        ) : (
+                            <Button buttonText={'팔로우하기'} size={'M'} onClickHandler={handleFollowClick} />
+                        )}
 
-            <ShareIcon src={shareIcon} alt="공유하기 아이콘" />
-          </>
-        )}
-      </ButtonsStyle>
-    </ProfileInfoContainer>
-  );
+                        <ShareIcon src={shareIcon} alt="공유하기 아이콘" />
+                    </>
+                )}
+            </ButtonsStyle>
+        </ProfileInfoContainer>
+    );
 }
 
 const ProfileInfoContainer = styled.section`
-  width: 100%;
-  height: 314px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: ${COLOR.bgPrimaryColor};
+    width: 100%;
+    height: 314px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: ${COLOR.bgPrimaryColor};
 
-  .a11y-hidden {
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-  }
+    .a11y-hidden {
+        clip: rect(1px, 1px, 1px, 1px);
+        clip-path: inset(50%);
+        width: 1px;
+        height: 1px;
+        margin: -1px;
+        overflow: hidden;
+        padding: 0;
+        position: absolute;
+    }
 `;
 const ProfileImgStyle = styled.div`
-  width: 100%;
+    width: 100%;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
 `;
 const ButtonsStyle = styled.div`
-  width: 100%;
+    width: 100%;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 const ProfileName = styled.h3`
-  color: ${COLOR.fontDarkColor};
-  font-size: ${FONT_SIZE.xlarge};
-  font-weight: 700;
-  margin-top: 16px;
+    color: ${COLOR.fontDarkColor};
+    font-size: ${FONT_SIZE.xlarge};
+    font-weight: 700;
+    margin-top: 16px;
 `;
 const ProfileId = styled.p`
-  color: ${COLOR.fontPrimaryColor};
-  font-size: ${FONT_SIZE.large};
-  font-weight: 400;
-  margin-top: 6px;
+    color: ${COLOR.fontPrimaryColor};
+    font-size: ${FONT_SIZE.large};
+    font-weight: 400;
+    margin-top: 6px;
 `;
 const ProfileMessage = styled.p`
-  color: ${COLOR.fontPrimaryColor};
-  font-size: ${FONT_SIZE.large};
-  font-weight: 400;
-  margin: 16px 0 24px 0;
+    color: ${COLOR.fontPrimaryColor};
+    font-size: ${FONT_SIZE.large};
+    font-weight: 400;
+    margin: 16px 0 24px 0;
 `;
 const ProfileImage = styled.img`
-  width: 110px;
-  height: 110px;
-  margin: 0 40px;
-  border-radius: 50%;
+    width: 110px;
+    height: 110px;
+    margin: 0 40px;
+    border-radius: 50%;
 `;
 const FollowInfoStyle = styled.div`
-  text-align: center;
-  font-size: 18px;
-  font-weight: 700;
-  display: flex;
-  flex-direction: column;
-  gap: 3px;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 700;
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
 `;
 const FollowInfoNumbers = styled.button`
-  font-size: 18px;
-  font-weight: 700;
-  color: ${(props) =>
-    props.isFollowing ? COLOR.fontDarkColor : COLOR.fontPrimaryColor};
-  display: block;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
+    font-size: 18px;
+    font-weight: 700;
+    color: ${(props) => (props.isFollowing ? COLOR.fontDarkColor : COLOR.fontPrimaryColor)};
+    display: block;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
 `;
 const FollowInfoText = styled.span`
-  color: ${COLOR.fontPrimaryColor};
-  font-size: 8px;
+    color: ${COLOR.fontPrimaryColor};
+    font-size: 8px;
 `;
 const ChatIcon = styled.img`
-  width: 18px;
-  height: 18px;
-  padding: 7px;
-  margin-right: 10px;
+    width: 18px;
+    height: 18px;
+    padding: 7px;
+    margin-right: 10px;
 
-  border: 1px solid ${COLOR.bgBorderColor};
-  border-radius: 50%;
+    border: 1px solid ${COLOR.bgBorderColor};
+    border-radius: 50%;
 `;
 const ShareIcon = styled.img`
-  width: 18px;
-  height: 18px;
-  padding: 7px;
-  margin-left: 10px;
-  border: 1px solid ${COLOR.bgBorderColor};
-  border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    padding: 7px;
+    margin-left: 10px;
+    border: 1px solid ${COLOR.bgBorderColor};
+    border-radius: 50%;
 `;
 const MyProfileBtns = styled.div`
-  display: flex;
-  gap: 12px;
+    display: flex;
+    gap: 12px;
 `;
--- src/pages/Profile/pages/EditProfile.jsx
+++ src/pages/Profile/pages/EditProfile.jsx
@@ -1,62 +1,58 @@
-import styled from "styled-components";
-import Button from "../../../components/Button";
-import Input from "../../../components/Input/Input";
-import Layout from "../../../components/Layout/Layout";
-import EditProfileIcon from "../../../assets/basic-profile-img.png";
+import styled from 'styled-components';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input/Input';
+import Layout from '../../../components/Layout/Layout';
+import { COLOR, FONT_SIZE } from '../../../utils';
+import EditProfileIcon from '../../../assets/basic-profile-img.png';
+import { useLocation } from 'react-router-dom';
+import AddImgIcon from '../../../assets/upload-file.png';
+import UploadHeader from '../../../components/Header/UploadHeader';
+import LayoutContent from '../../../components/Layout/LayoutContent';
 
 export default function EditProfile() {
-  return (
-    <Layout>
-      <EditProfilePage>
-        <Title>프로필 설정</Title>
-        <Text>나중에 언제든지 변경할 수 있습니다.</Text>
-        <ProfileImg src={EditProfileIcon} />
-        <Input
-          type="text"
-          labelText="사용자이름"
-          placeholder="2-10자 이내여야 합니다."
-        />
-        <Input
-          type="text"
-          labelText="계정 ID"
-          placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-        />
-        <Input
-          type="text"
-          labelText="소개"
-          placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
-        />
-        <Button buttonText="감귤마켓 시작하기" disabled />
-      </EditProfilePage>
-    </Layout>
-  );
+    const location = useLocation();
+
+    return (
+        <Layout>
+            <UploadHeader buttonText={'저장'} disabled size={'MS'} onClickHandler={() => {}} />
+            <LayoutContent>
+                <EditProfileStyle>
+                    <ProfileImageStyle>
+                        <ProfileImg src={EditProfileIcon} />
+                        <AddImg src={AddImgIcon} />
+                    </ProfileImageStyle>
+
+                    <Input type="text" labelText="사용자이름" placeholder="2-10자 이내여야 합니다." placeholderColor={COLOR.bgBorderColor} onChangeHandler={() => {}} />
+                    <Input type="text" labelText="계정 ID" placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다." placeholderColor={COLOR.bgBorderColor} onChangeHandler={() => {}} />
+                    <Input type="text" labelText="소개" placeholder="자신과 판매할 상품에 대해 소개해 주세요!" placeholderColor={COLOR.bgBorderColor} onChangeHandler={() => {}} />
+                </EditProfileStyle>
+            </LayoutContent>
+        </Layout>
+    );
 }
 
-const EditProfilePage = styled.div`
-  margin: 10%;
-  & > Button {
-    margin: 0 auto;
-    margin-top: 30px;
-  }
-`;
-
-const Title = styled.div`
-  font-size: 24px;
-  color: #000000;
-  text-align: center;
-  margin-bottom: 15px;
+const EditProfileStyle = styled.div`
+    margin: 30px 18px 0;
 `;
 
 const ProfileImg = styled.img`
-  display: block;
-  width: 110px;
-  height: 110px;
-  margin: 0 auto;
-  margin-bottom: 30px;
+    display: block;
+    width: 110px;
+    height: 110px;
+    margin: 0 auto;
+    margin-bottom: 30px;
 `;
 
-const Text = styled.div`
-  font-size: 14px;
-  text-align: center;
-  margin-bottom: 30px;
+const ProfileImageStyle = styled.div`
+    position: relative;
+`;
+
+const AddImg = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 18px;
+    position: absolute;
+    bottom: 0;
+    left: 55%;
+    cursor: pointer;
 `;
