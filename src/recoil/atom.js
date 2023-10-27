814ddd28eb7c2b9f246297c3c150d31b9dcf7d73
oqoqoh <oqoq59@gmail.com>
2023-10-28 02:59:08 +0900

    Docs 📝: Recoil 이미지 경로 수정

--- src/recoil/atom.js
+++ src/recoil/atom.js
@@ -8,7 +8,7 @@ export const userState = atom({
         email: '10duck@test.com',
         accountname: '10duck',
         intro: '히사시부리랄까...!',
-        image: 'https://api.mandarin.weniv.co.kr/1698166471346.jpeg',
+        image: 'https://api.mandarin.weniv.co.kr/1698429305810.png',
         token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MmY4YmQ1YjJjYjIwNTY2MzdjNDZmNiIsImV4cCI6MTcwMzU3MjE2OCwiaWF0IjoxNjk4Mzg4MTY4fQ.R1HOkmM18OiWuI1vrQ5ji0rWrYNu1HriCWOZod33O98',
         refreshToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTgzODgxNjgsImV4cCI6MTY5OTU5Nzc2OH0.8OApb9sPOf07GPcT5RU4tc3jkpGtQWVMsSi_Z65V7tM',
     },
