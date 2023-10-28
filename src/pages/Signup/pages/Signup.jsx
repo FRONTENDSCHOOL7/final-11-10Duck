3af328909fe2256eb622178eeb0cab5d0a356130
seoyeon.lee <ivyseo0312@korea.ac.kr>
2023-10-28 21:30:17 +0900

    Refactor ♻️:  주석 삭제

--- src/pages/Signup/pages/Signup.jsx
+++ src/pages/Signup/pages/Signup.jsx
@@ -1,95 +1,3 @@
-// import styled from "styled-components";
-// import Layout from "../../../components/Layout/Layout";
-// import Input from "../../../components/Input/Input";
-// import Button from "../../../components/Button";
-// import { useEffect, useState } from "react";
-// import { COLOR, FONT_SIZE } from "../../../utils";
-// import { api } from "../../../api/baseURL";
-// import { useNavigate } from "react-router-dom";
-
-// export default function Signup() {
-//   const [user, setUser] = useState({
-//     email: "",
-//     password: "",
-//   });
-//   console.log(user);
-//   const [errorMsg, setErrorMsg] = useState("");
-//   const [disabledBtn, setDisabledBtn] = useState(false);
-
-//   const navigate = useNavigate();
-
-//   function validationCheck() {
-//     const checkemail = localStorage.getItem("user");
-//     console.log(checkemail);
-//     if (user.password.length < 6) {
-//       setDisabledBtn(true);
-//       setErrorMsg("비밀번호를 6자이상 입력해주세요.");
-//     } else if (user.email === checkemail.email) {
-//       setDisabledBtn(true);
-//       setErrorMsg("중복된 이메일입니다.");
-//     } else {
-//       setDisabledBtn(false);
-//       setErrorMsg("");
-//     }
-//   }
-
-//   useEffect(() => {
-//     setDisabledBtn(!(user.email && user.password));
-//   }, [user]);
-
-//   return (
-//     <Layout>
-//       <SignupPage>
-//         <Title>이메일로 회원가입</Title>
-//         <Input
-//           name="email"
-//           labelText="이메일"
-//           placeholder="이메일 주소를 입력해 주세요."
-//           placeholderColor={COLOR.fontLightGrayColor}
-//           onChangeHandler={(event) => {
-//             setUser({ ...user, email: event.target.value });
-//           }}
-//         />
-//         <Input
-//           name="password"
-//           type="password"
-//           labelText="비밀번호"
-//           placeholder="비밀번호를 설정해 주세요."
-//           placeholderColor={COLOR.fontLightGrayColor}
-//           onChangeHandler={(event) => {
-//             setUser({ ...user, password: event.target.value });
-//             validationCheck();
-//           }}
-//           alert={errorMsg}
-//         />
-//         <Button
-//           buttonText="다음"
-//           disabled={disabledBtn}
-//           onClickHandler={() => {
-//             navigate("/signup/edit-profile", { state: { user } });
-//           }}
-//           reversed={false}
-//         />
-//       </SignupPage>
-//     </Layout>
-//   );
-// }
-
-// const SignupPage = styled.div`
-//   margin: 10%;
-//   & > Button {
-//     font-size: ${FONT_SIZE.large};
-//     margin: 0 auto;
-//     margin-top: 30px;
-//   }
-// `;
-
-// const Title = styled.div`
-//   font-size: 24px;
-//   color: #000000;
-//   text-align: center;
-//   margin-bottom: 40px;
-// `;
 import styled from "styled-components";
 import Layout from "../../../components/Layout/Layout";
 import Input from "../../../components/Input/Input";
@@ -174,9 +82,9 @@ const SignupPage = styled.div`
 `;
 
 const Title = styled.div`
-    font-size: 24px;
-    color: #000000;
-    text-align: center;
-    margin-bottom: 40px;
-  };
-  `;
+      font-size: 24px;
+      color: #000000;
+      text-align: center;
+      margin-bottom: 40px;
+    };
+    `;
