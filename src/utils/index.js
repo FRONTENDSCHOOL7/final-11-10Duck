6d3093dcde44c9a1ac6bf7cb997448ad13b1dc7f
oqoqoh <oqoq59@gmail.com>
2023-10-31 15:39:59 +0900

    Docs 📝 : 기본 프로필 이미지 경로 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,29 +1,34 @@
 export const COLOR = {
-  borderPrimaryColor: "#DBDBDB",
-  borderSecondaryColor: "#F26E22",
-  borderOrangeColor: "#F26E22",
+    borderPrimaryColor: '#DBDBDB',
+    borderSecondaryColor: '#F26E22',
+    borderOrangeColor: '#F26E22',
 
-  bgPrimaryColor: "#FFFFFF",
-  bgSecondaryColor: "#F2F2F2",
-  bgOrangeColor: "#EA7F42",
-  bgBorderColor: `#DBDBDB`,
+    bgPrimaryColor: '#FFFFFF',
+    bgSecondaryColor: '#F2F2F2',
+    bgOrangeColor: '#EA7F42',
+    bgBorderColor: `#DBDBDB`,
 
-  btnPrimaryColor: "#F26E22",
-  btnSecondaryColor: "#FFFFFF",
-  btnDisabledColor: "#FFC7A7",
+    btnPrimaryColor: '#F26E22',
+    btnSecondaryColor: '#FFFFFF',
+    btnDisabledColor: '#FFC7A7',
 
-  fontPrimaryColor: "#767676",
-  fontSecondaryColor: "#FFFFFF",
-  fontDarkColor: "#000000",
-  fontLightGrayColor: "#DBDBDB",
-  fontOrangeColor: "#F26E22",
-  fontDarkGrayColor: "#333333",
-  fontRedColor: "#EB5757",
+    fontPrimaryColor: '#767676',
+    fontSecondaryColor: '#FFFFFF',
+    fontDarkColor: '#000000',
+    fontLightGrayColor: '#DBDBDB',
+    fontOrangeColor: '#F26E22',
+    fontDarkGrayColor: '#333333',
+    fontRedColor: '#EB5757',
 };
 
 export const FONT_SIZE = {
-  small: "10px",
-  medium: "12px",
-  large: "14px",
-  xlarge: "16px",
+    small: '10px',
+    medium: '12px',
+    large: '14px',
+    xlarge: '16px',
+};
+
+export const FILE = {
+    defaultProfileImage: '1698733625783.png',
+    defaultProfileImageUrl: 'https://api.mandarin.weniv.co.kr/1698733625783.png',
 };
