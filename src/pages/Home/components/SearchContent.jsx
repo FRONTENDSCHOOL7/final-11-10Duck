81ad518976305fd5dc1c5c010627aa00ce9f9011
oqoqoh <oqoq59@gmail.com>
2023-10-31 18:08:11 +0900

    Feat ➕ : 검색페이지, 검색어 입력시 해당 유저 조회 기능 추가

--- src/pages/Home/components/SearchContent.jsx
+++ src/pages/Home/components/SearchContent.jsx
@@ -1,48 +1,49 @@
-import { styled } from "styled-components";
-import { FONT_SIZE, COLOR } from "../../../utils";
+import { styled } from 'styled-components';
+import { FONT_SIZE, COLOR } from '../../../utils';
 
 export default function SearchContent(props) {
-  const { user } = props;
+    const { user } = props;
 
-  const { image, userName, id } = user;
-
-  return (
-    <DataStyle>
-      <ImgStyle src={image} alt="프로필 이미지" />
-      <div className="ContentStyle">
-        <span>{userName}</span>
-        <span>{id}</span>
-      </div>
-    </DataStyle>
-  );
+    return (
+        <DataStyle>
+            <ImgStyle src={user.image} alt="프로필 이미지" />
+            <div className="ContentStyle">
+                <span>{user.username}</span>
+                <span>@{user.accountname}</span>
+            </div>
+        </DataStyle>
+    );
 }
 
 const DataStyle = styled.div`
-  display: flex;
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
 
-  & > .ContentStyle {
-    vertical-align: middle;
-  }
+    & > .ContentStyle {
+        vertical-align: middle;
+    }
 
-  & > .ContentStyle > span {
-    display: block;
-  }
+    & > .ContentStyle > span {
+        display: block;
+    }
 
-  & > .ContentStyle > span:first-child {
-    font-size: ${FONT_SIZE.large};
-    margin-bottom: 6px;
-    color: ${COLOR.fontDarkColor};
-  }
+    & > .ContentStyle > span:first-child {
+        font-size: ${FONT_SIZE.large};
+        margin-bottom: 6px;
+        color: ${COLOR.fontDarkColor};
+    }
 
-  & > .ContentStyle > span:last-child {
-    font-size: ${FONT_SIZE.medium};
-    color: ${COLOR.fontPrimaryColor};
-  }
+    & > .ContentStyle > span:last-child {
+        font-size: ${FONT_SIZE.medium};
+        color: ${COLOR.fontPrimaryColor};
+    }
 `;
 
 const ImgStyle = styled.img`
-  width: 50px;
-  height: 50px;
-  object-fit: cover;
-  margin-right: 12px;
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    margin-right: 12px;
+    border-radius: 50%;
 `;
--- src/pages/Home/pages/Search.jsx
+++ src/pages/Home/pages/Search.jsx
@@ -1,14 +1,63 @@
-import Layout from "../../../components/Layout/Layout";
-import LayoutContent from "../../../components/Layout/LayoutContent";
-import SearchHeader from "../../../components/Header/SearchHeader";
-import NavBar from "../../../components/Footer/NavBar";
+import { styled } from 'styled-components';
+import Layout from '../../../components/Layout/Layout';
+import LayoutContent from '../../../components/Layout/LayoutContent';
+import SearchHeader from '../../../components/Header/SearchHeader';
+import NavBar from '../../../components/Footer/NavBar';
+import SearchContent from '../components/SearchContent';
+import ProfileImg from '../../../assets/profile-example.png';
+import { useEffect, useState } from 'react';
+import useAPI from '../../../hooks/useAPI';
+import { api } from '../../../api/baseURL';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../../recoil/atom';
 
 export default function Search() {
-  return (
-    <Layout>
-      <SearchHeader />
-      <LayoutContent></LayoutContent>
-      <NavBar />
-    </Layout>
-  );
+    const { header } = useAPI();
+    //const user = useRecoilValue(userState);
+
+    const [searchInput, setSearchInput] = useState('');
+    const [searchList, setSearchList] = useState([]);
+
+    const fetchSearchUser = async () => {
+        try {
+            const res = await api.get(`/user/searchuser/?keyword=${searchInput}`, {
+                headers: header,
+            });
+            const resList = res.data.filter((data) => data.username.includes(searchInput) || data.accountname.includes(searchInput));
+
+            // const res = await api.get(`/user/searchuser/?keyword=${'ssduck'}`, {
+            //     headers: header,
+            // });
+            // const resList = res.data.filter((data) => data.username.includes('ssduck') || data.accountname.includes('ssduck'));
+
+            setSearchList([...resList]);
+
+            console.log('🌟유저 검색 성공');
+        } catch (err) {
+            console.error(err);
+            console.log('🔥유저 검색 실패');
+        }
+    };
+
+    useEffect(() => {
+        fetchSearchUser();
+    }, [searchInput]);
+    return (
+        <Layout>
+            <SearchHeader setSearchInput={setSearchInput} />
+            <LayoutContent>
+                <SearchStyle>
+                    {searchList.map((user) => {
+                        return <SearchContent user={user} />;
+                    })}
+                </SearchStyle>
+            </LayoutContent>
+            <NavBar />
+        </Layout>
+    );
 }
+
+const SearchStyle = styled.div`
+    margin-top: 20px;
+    display: block;
+`;
