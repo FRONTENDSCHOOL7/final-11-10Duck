013f68d48801dbf4c5124857c3453de15d6df34b
oqoqoh <oqoq59@gmail.com>
2023-11-02 16:55:49 +0900

    Bug 🐛 : 회원가입시 기본 이미지 이슈 수정

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,34 +1,34 @@
 export const COLOR = {
-  borderPrimaryColor: "#DBDBDB",
-  borderSecondaryColor: "#883BC5",
-  borderOrangeColor: "#883BC5",
+    borderPrimaryColor: '#DBDBDB',
+    borderSecondaryColor: '#883BC5',
+    borderOrangeColor: '#883BC5',
 
-  bgPrimaryColor: "#FBF7FF",
-  bgSecondaryColor: "#F2F2F2",
-  bgOrangeColor: "#883BC5",
-  bgBorderColor: `#DBDBDB`,
+    bgPrimaryColor: '#FBF7FF',
+    bgSecondaryColor: '#F2F2F2',
+    bgOrangeColor: '#883BC5',
+    bgBorderColor: `#DBDBDB`,
 
-  btnPrimaryColor: "#883BC5",
-  btnSecondaryColor: "#FBF7FF",
-  btnDisabledColor: "#D9A8FF",
+    btnPrimaryColor: '#883BC5',
+    btnSecondaryColor: '#FBF7FF',
+    btnDisabledColor: '#D9A8FF',
 
-  fontPrimaryColor: "#767676",
-  fontSecondaryColor: "#FBF7FF",
-  fontDarkColor: "#000000",
-  fontLightGrayColor: "#DBDBDB",
-  fontOrangeColor: "#883BC5",
-  fontDarkGrayColor: "#333333",
-  fontRedColor: "#EB5757",
+    fontPrimaryColor: '#767676',
+    fontSecondaryColor: '#FBF7FF',
+    fontDarkColor: '#000000',
+    fontLightGrayColor: '#DBDBDB',
+    fontOrangeColor: '#883BC5',
+    fontDarkGrayColor: '#333333',
+    fontRedColor: '#EB5757',
 };
 
 export const FONT_SIZE = {
-  small: "10px",
-  medium: "12px",
-  large: "14px",
-  xlarge: "16px",
+    small: '10px',
+    medium: '12px',
+    large: '14px',
+    xlarge: '16px',
 };
 
 export const FILE = {
-  defaultProfileImage: "1698733625783.png",
-  defaultProfileImageUrl: "https://api.mandarin.weniv.co.kr/1698733625783.png",
+    defaultProfileImage: '1698904346268.png',
+    defaultProfileImageUrl: 'https://api.mandarin.weniv.co.kr/1698904346268.png',
 };
