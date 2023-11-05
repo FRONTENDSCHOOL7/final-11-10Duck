566a361de8e8d939f77c69f4afe34612ed164fd8
oqoqoh <oqoq59@gmail.com>
2023-11-05 16:29:46 +0900

    Refactor ♻️ : 검색, 둘러보기 리펙토링

--- src/pages/Home/pages/Search.jsx
+++ src/pages/Home/pages/Search.jsx
@@ -1,69 +1,56 @@
-import { styled } from 'styled-components';
-import Layout from '../../../components/Layout/Layout';
-import LayoutContent from '../../../components/Layout/LayoutContent';
-import SearchHeader from '../../../components/Header/SearchHeader';
-import NavBar from '../../../components/Footer/NavBar';
-import SearchContent from '../components/SearchContent';
-import { useEffect, useState } from 'react';
-import useAPI from '../../../hooks/useAPI';
-import { api } from '../../../api/baseURL';
+import { styled } from "styled-components";
+import Layout from "../../../components/Layout/Layout";
+import LayoutContent from "../../../components/Layout/LayoutContent";
+import SearchHeader from "../../../components/Header/SearchHeader";
+import NavBar from "../../../components/Footer/NavBar";
+import SearchContent from "../components/SearchContent";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { ssduckUserListState } from "../../../recoil/atom";
 
 export default function Search() {
-    const { header } = useAPI();
+  const ssduckUserList = useRecoilValue(ssduckUserListState);
 
-    const [searchInput, setSearchInput] = useState('');
-    const [searchList, setSearchList] = useState([]);
-    const [allUserList, setAllUserList] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchList, setSearchList] = useState([]);
 
-    const fetch10DuckUsers = async () => {
-        try {
-            const res = await api.get(`user/searchuser/?keyword=ssduck`, {
-                headers: header,
-            });
+  const searchUsers = () => {
+    const res = ssduckUserList.filter((user) => {
+      return (
+        user.username.includes(searchInput) ||
+        user.accountname.includes(searchInput)
+      );
+    });
+    setSearchList(res);
+  };
 
-            res.data && res.data.forEach((user) => {
-                user.accountname.includes('ssduck') && setAllUserList((prev) => [...prev, user]);
-            });
+  useEffect(() => {
+    searchUsers();
+  }, [searchInput]);
 
-            console.log('🌟씁덕학개론 유저 목록 불러오기 성공');
-        } catch (err) {
-            console.error(err);
-            console.log('🔥씁덕학개론 유저 목록 불러오기 실패');
-        }
-    };
-
-    const searchUsers = () => {
-        const res = allUserList.filter((user)=>{
-            return user.username.includes(searchInput) || user.accountname.includes(searchInput);
-        })
-        setSearchList(res)
-    }
-
-    useEffect(()=>{
-        fetch10DuckUsers();
-    },[])
-
-    useEffect(() => {
-        searchUsers();
-    }, [searchInput]);
-    
-    return (
-        <Layout>
-            <SearchHeader setSearchInput={setSearchInput} />
-            <LayoutContent>
-                <SearchStyle>
-                    {searchInput.length > 0 &&
-                        searchList.map((user) => {
-                            return <SearchContent user={user} searchInput={searchInput} key={user._id}/>;
-                        })}
-                </SearchStyle>
-            </LayoutContent>
-            <NavBar />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <SearchHeader setSearchInput={setSearchInput} />
+      <LayoutContent>
+        <SearchStyle>
+          {searchInput.length > 0 &&
+            searchList.map((user) => {
+              return (
+                <SearchContent
+                  user={user}
+                  searchInput={searchInput}
+                  key={user._id}
+                />
+              );
+            })}
+        </SearchStyle>
+      </LayoutContent>
+      <NavBar />
+    </Layout>
+  );
 }
 
 const SearchStyle = styled.div`
-    margin-top: 20px;
-    display: block;
+  margin-top: 20px;
+  display: block;
 `;
--- src/pages/LookAround/pages/LookAround.jsx
+++ src/pages/LookAround/pages/LookAround.jsx
@@ -1,133 +1,131 @@
-import React, { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
-import { COLOR } from '../../../utils';
-import Layout from '../../../components/Layout/Layout';
-import BasicHeader from '../../../components/Header/BasicHeader';
-import LayoutContent from '../../../components/Layout/LayoutContent';
-import NavBar from '../../../components/Footer/NavBar';
-import Gallery from '../components/Gallery';
-import { api } from '../../../api/baseURL';
-import useAPI from '../../../hooks/useAPI';
-import useModal from '../../../hooks/useModal';
-import BottomModal from '../../../components/Modal/BottomModal';
-import AlertModal from '../../../components/Modal/AlertModal';
-import useAlertModal from '../../../hooks/useAlertModal';
-import { useSetRecoilState } from 'recoil';
-import { userState } from '../../../recoil/atom';
-import { useNavigate } from 'react-router-dom';
+import React, { Suspense, useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { COLOR } from "../../../utils";
+import Layout from "../../../components/Layout/Layout";
+import BasicHeader from "../../../components/Header/BasicHeader";
+import LayoutContent from "../../../components/Layout/LayoutContent";
+import NavBar from "../../../components/Footer/NavBar";
+import Gallery from "../components/Gallery";
+import { api } from "../../../api/baseURL";
+import useAPI from "../../../hooks/useAPI";
+import useModal from "../../../hooks/useModal";
+import BottomModal from "../../../components/Modal/BottomModal";
+import AlertModal from "../../../components/Modal/AlertModal";
+import useAlertModal from "../../../hooks/useAlertModal";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { ssduckUserListState, userState } from "../../../recoil/atom";
+import { useNavigate } from "react-router-dom";
+import Loading from "../../../components/Loading/Loading";
 
 export default function LookAround() {
-    const { header } = useAPI();
-    const setUser = useSetRecoilState(userState);
-    const navigate = useNavigate();
+  const { header } = useAPI();
+  const setUser = useSetRecoilState(userState);
+  const ssduckUsers = useRecoilValue(ssduckUserListState);
 
-    const { isModalOpen, isUserAlertModalOpen, userAlertModal, userModalMenuList, onModalHandler, userAlertModalHandler } = useModal();
-    const { isAlertModalOpen, alertModalHandler } = useAlertModal();
+  const navigate = useNavigate();
 
-    const [userList, setUserList] = useState([]);
-    const [galleyList, setGalleryList] = useState([]);
-    const [alertModal, setAlertModal] = useState({
-        alertTitle: '',
-        leftBtnText: '취소',
-        rightBtnText: '',
-    });
+  const {
+    isModalOpen,
+    isUserAlertModalOpen,
+    userAlertModal,
+    userModalMenuList,
+    onModalHandler,
+    userAlertModalHandler,
+  } = useModal();
+  const { isAlertModalOpen, alertModalHandler } = useAlertModal();
 
-    const fetch10DuckUsers = async () => {
-        try {
-            const res = await api.get(`user/searchuser/?keyword=ssduck`, {
-                headers: header,
-            });
+  const [galleyList, setGalleryList] = useState([]);
+  const [alertModal, setAlertModal] = useState({
+    alertTitle: "",
+    leftBtnText: "취소",
+    rightBtnText: "",
+  });
 
-            res.data && res.data.forEach((user) => {
-                user.accountname.includes('ssduck') && setUserList((prev) => [...prev, user]);
-            });
+  const fetchUserPost = async (userAccountname) => {
+    try {
+      const res = await api.get(`/post/${userAccountname}/userpost`, {
+        headers: header,
+      });
 
-            console.log('🌟씁덕학개론 유저 목록 불러오기 성공');
-        } catch (err) {
-            console.error(err);
-            console.log('🔥씁덕학개론 유저 목록 불러오기 실패');
-        }
-    };
-    const fetchUserPost = async (userAccountname) => {
-        try {
-            const res = await api.get(`/post/${userAccountname}/userpost`, {
-                headers: header,
-            });
-
-            res.data && res.data.post.forEach((post) => {
-                post.image && setGalleryList((prev) => [...prev, post]);
-            });
-
-            console.log(`🌟${userAccountname} 게시글 불러오기 성공`);
-        } catch (err) {
-            console.error(err);
-            console.log(`🔥${userAccountname} 게시글 불러오기 실패`);
-        }
-    };
-
-    const sortShuffle = (arr) => {
-        return arr.sort(() => Math.random() - 0.5);
-    };
-
-    useEffect(() => {
-        !userList.length && fetch10DuckUsers();
-    }, []);
-
-    useEffect(() => {
-        sortShuffle(userList).forEach((user) => {
-            fetchUserPost(user.accountname);
+      res.data &&
+        res.data.post.forEach((post) => {
+          post.image && setGalleryList((prev) => [...prev, post]);
         });
-    }, [userList]);
 
-    return (
-        <Layout>
-            <BasicHeader mode={'post'} onClickMoreBtnHandler={onModalHandler} />
-            <LayoutContent>
-                <GalleryStyle>
-                    {galleyList && galleyList.map((post) => {
-                        return <Gallery key={post.id} post={post} />;
-                    })}
-                </GalleryStyle>
-            </LayoutContent>
-            <BottomModal isModalOpen={isModalOpen} menu={userModalMenuList} onModalHandler={onModalHandler} />
-            <AlertModal
-                isModalOpen={isAlertModalOpen}
-                alertTitle={alertModal.alertTitle}
-                leftBtnText={alertModal.leftBtnText}
-                rightBtnText={alertModal.rightBtnText}
-                onModalHandler={alertModalHandler}
-            />
-            <AlertModal
-                isModalOpen={isUserAlertModalOpen}
-                alertTitle={userAlertModal.alertTitle}
-                leftBtnText={userAlertModal.leftBtnText}
-                rightBtnText={userAlertModal.rightBtnText}
-                onModalHandler={userAlertModalHandler}
-                onClickRightBtnHandler={() => {
-                    setUser({
-                        _id: '',
-                        username: '',
-                        email: '',
-                        accountname: '',
-                        intro: '',
-                        image: '',
-                        token: '',
-                        refreshToken: '',
-                    });
-                    localStorage.removeItem('token');
-                    navigate('/');
-                }}
-            />
-            <NavBar />
-        </Layout>
-    );
+      console.log(`🌟${userAccountname} 게시글 불러오기 성공`);
+    } catch (err) {
+      console.error(err);
+      console.log(`🔥${userAccountname} 게시글 불러오기 실패`);
+    }
+  };
+
+  const sortShuffle = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+  };
+
+  useEffect(() => {
+    ssduckUsers.forEach((user) => {
+      fetchUserPost(user.accountname);
+    });
+  }, [ssduckUsers]);
+
+  return (
+    <Layout>
+      <BasicHeader mode={"post"} onClickMoreBtnHandler={onModalHandler} />
+      <LayoutContent>
+        <GalleryStyle>
+          <Suspense fallback={<Loading />}>
+            {galleyList &&
+              sortShuffle(galleyList).map((post) => {
+                return <Gallery key={post.id} post={post} />;
+              })}
+          </Suspense>
+        </GalleryStyle>
+      </LayoutContent>
+      <BottomModal
+        isModalOpen={isModalOpen}
+        menu={userModalMenuList}
+        onModalHandler={onModalHandler}
+      />
+      <AlertModal
+        isModalOpen={isAlertModalOpen}
+        alertTitle={alertModal.alertTitle}
+        leftBtnText={alertModal.leftBtnText}
+        rightBtnText={alertModal.rightBtnText}
+        onModalHandler={alertModalHandler}
+      />
+      <AlertModal
+        isModalOpen={isUserAlertModalOpen}
+        alertTitle={userAlertModal.alertTitle}
+        leftBtnText={userAlertModal.leftBtnText}
+        rightBtnText={userAlertModal.rightBtnText}
+        onModalHandler={userAlertModalHandler}
+        onClickRightBtnHandler={() => {
+          setUser({
+            _id: "",
+            username: "",
+            email: "",
+            accountname: "",
+            intro: "",
+            image: "",
+            token: "",
+            refreshToken: "",
+          });
+          localStorage.removeItem("token");
+          navigate("/");
+        }}
+      />
+      <NavBar />
+      <Loading />
+    </Layout>
+  );
 }
 
 const GalleryStyle = styled.section`
-    display: grid;
-    place-content: center;
-    grid-template-columns: repeat(3, minmax(auto, 114px));
-    gap: 5px;
-    padding: 16px 0 30px;
-    background-color: ${COLOR.bgPrimaryColor};
+  display: grid;
+  place-content: center;
+  grid-template-columns: repeat(3, minmax(auto, 114px));
+  gap: 5px;
+  padding: 16px 0 30px;
+  background-color: ${COLOR.bgPrimaryColor};
 `;
